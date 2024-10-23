04f8871cdcf7967e3534a16561540c05f299f45e
phamthimylinhhaui <phamthimylinhhaui@gmail.com>
2024-10-23 22:28:40 +0700

    lesson-167

--- section-14/lesson-165-promise.js
+++ section-14/lesson-165-promise.js
@@ -12,3 +12,19 @@ console.log(2)
 // 2 1 callback gop phan biet ham có thao tác bất đồng bộ khi nào đã được thực hiện
 
 // 1 số phương thức asyn: setTimeout, setInterval, fetch, xmlHTTP, đọc file ..
+
+// -------------- pain ------------
+// callback hell: dữ liệu bị rằng buộc sau phụ thuộc vào trước
+// pyramid dom
+
+setTimeout(() => {
+    console.log('viec 1');
+
+    setTimeout(() => {
+        console.log('viec 2')
+
+        setTimeout(() => {
+            console.log('viec 3')
+        }, 1000)
+    }, 1000)
+}, 1000)// callback hell
