2d3ba106a332813c998e8719300d8fc5bcf939ac
nấm <87581534+phamthimylinhhaui@users.noreply.github.com>
2024-10-23 22:10:01 +0700

    lesson-165-2 (#2)

--- section-14/lesson-165-promise.js
+++ section-14/lesson-165-promise.js
@@ -1,1 +1,14 @@
+// Overview
+// - Sync
+// - Async
+// - Nỗi đau
+// - Lý thuyết và cách hoạt động
+// - Thực hành ví dụ
 
+// sync/async
+setTimeout(() => {
+    console.log(1)}, 100)
+console.log(2)
+// 2 1 callback gop phan biet ham có thao tác bất đồng bộ khi nào đã được thực hiện
+
+// 1 số phương thức asyn: setTimeout, setInterval, fetch, xmlHTTP, đọc file ..
