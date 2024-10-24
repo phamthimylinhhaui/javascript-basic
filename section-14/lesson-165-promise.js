ce8cd76dc863423eca875f592fae5992e30e46f9
phamthimylinhhaui <phamthimylinhhaui@gmail.com>
2024-10-24 22:30:26 +0700

    lesson-168: concept

--- section-14/lesson-165-promise.js
+++ section-14/lesson-165-promise.js
@@ -7,7 +7,8 @@
 
 // sync/async
 setTimeout(() => {
-    console.log(1)}, 100)
+    console.log(1)
+}, 100)
 console.log(2)
 // 2 1 callback gop phan biet ham có thao tác bất đồng bộ khi nào đã được thực hiện
 
@@ -29,4 +30,29 @@ setTimeout(() => {
     }, 1000)
 }, 1000)// callback hell
 
-// concept
+// concept: cách sử dụng
+// dùng promise thông qua đối tượng Promise contructor của js
+
+// 1 promise khi thực thi sẽ có 3 trạng thái
+//  - pending (đang thực hiện và executor không trả về resolve hay reject)
+//  - fulfilled: (resolve)
+//  - reject: (reject)
+
+const promise = new Promise(
+    // func executor: được thực hiện luôn
+    function (resolve, reject) {
+    //   trong này sẽ thực hiện logic để có thể trả ra resolve(value) hoặc reject(value)
+    //     resolve(['value'])
+        resolve('message error')
+    });
+
+promise.then(function (result) {
+    console.log('executor goi den resolve:', result)
+})
+    .catch(function (err) {
+        console.log('executor goi den reject:', err)
+    })
+    .finally(function () {
+        console.log('executor goi den resolve hoac reject')
+    })
+
